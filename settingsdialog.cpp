2be3fa7ec79b75678e44beaa88d1d1bc1e306357
Rondir <34924974+Rondir@users.noreply.github.com>
2017-12-28 23:31:56 +0100

    Add files via upload

--- settingsdialog.cpp
+++ settingsdialog.cpp
@@ -2,40 +2,40 @@
 
 SettingsDialog::SettingsDialog(QWidget *parent) : QDialog(parent), ui(new Ui::SettingsDialog)
 {
-  ui->setupUi(this);
+	ui->setupUi(this);
 }
 
 SettingsDialog::~SettingsDialog()
 {
-  delete ui;
+	delete ui;
 }
 
 void SettingsDialog::ToggleShowHide()
 {
-  if (!isVisible())
-  {
-    setVisible(true);
-  }
-  else
-    setVisible(false);
+	if (!isVisible())
+	{
+		setVisible(true);
+	}
+	else
+		setVisible(false);
 }
 
 void SettingsDialog::btn_SetHeroesGameScene_clicked()
 {
-  if ((ui->list_HeroesScenes->selectedItems().count() > 0) &&
-    (ui->list_HeroesScenes->currentItem()->text() != ui->eb_HeroesMenuScene->text()))
-  {
-    QString selectedScene = ui->list_HeroesScenes->currentItem()->text();
-    ui->eb_HeroesGameScene->setText(selectedScene);
-  }
+	if ((ui->list_HeroesScenes->selectedItems().count() > 0) &&
+		(ui->list_HeroesScenes->currentItem()->text() != ui->eb_HeroesMenuScene->text()))
+	{
+		QString selectedScene = ui->list_HeroesScenes->currentItem()->text();
+		ui->eb_HeroesGameScene->setText(selectedScene);
+	}
 }
 
 void SettingsDialog::btn_SetHeroesMenuScene_clicked()
 {
-  if ((ui->list_HeroesScenes->selectedItems().count() > 0) &&
-    (ui->list_HeroesScenes->currentItem()->text() != ui->eb_HeroesGameScene->text()))
-  {
-    QString selectedScene = ui->list_HeroesScenes->currentItem()->text();
-    ui->eb_HeroesMenuScene->setText(selectedScene);
-  }
+	if ((ui->list_HeroesScenes->selectedItems().count() > 0) &&
+		(ui->list_HeroesScenes->currentItem()->text() != ui->eb_HeroesGameScene->text()))
+	{
+		QString selectedScene = ui->list_HeroesScenes->currentItem()->text();
+		ui->eb_HeroesMenuScene->setText(selectedScene);
+	}
 }
